e24580eb4fbbf2fefef0928f2f16a4ee567db34b
JackCChen2017 <jackchenxing@live.com>
2019-11-27 00:29:24 +0800

    add more function

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+from random import choice
+
+
+class Person:
+    def __init__(self, name):
+        self.greeting = 'hello {name}'
+        self.name = name
+
+    def __str__(self):
+        return self.make_greeting()
+
+    def make_greeting(self):
+        return self.greeting.format(name=self.name)
+
+
+def main():
+    people = [
+        Person('jane'),
+        Person('jill'),
+        Person('bob')
+    ]
+
+    person = choice(people)
+    print(person)
 
 
 if __name__ == '__main__':
-    pass
+    main()
